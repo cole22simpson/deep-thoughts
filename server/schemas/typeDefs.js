b4672ba7a6a4ef2262bda72b63b35256f7bcb636
Cole Simpson <cole22simpson@gmail.com>
2022-06-29 16:29:46 -0700

    idk

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,23 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+
+  query {
+    thoughts {
+      _id: ID
+      thoughtText: String
+      createdAt: String
+      username: String
+      reactionCount: Int
+    }
+  }
+
+  type Query {
+    thoughts(username: String): [Thought]
+  }
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
